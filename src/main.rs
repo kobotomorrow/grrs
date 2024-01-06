e9b5e686090e4703e3bc8f887904264d0a7fd58d
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2024-01-06 15:24:09 +0900

    refactor: find_match関数をlibに切り出す

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use anyhow::{Context, Result};
-use std::{path, fs::{self}, io::{BufReader, BufRead, BufWriter, stdout, Write}};
+use std::{path, fs::{self}, io::{BufReader, BufRead, BufWriter, stdout}};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
@@ -9,28 +9,6 @@ struct Cli {
     pattern: String,
     /// The path to the file to read
     path: path::PathBuf
-}
-
-fn find_matches(mut writer: impl Write, lines: Vec<String>, pattern: &str) -> Result<()> {
-    for line in lines {
-        if line.contains(pattern) {
-            writeln!(writer, "{}", line)?;
-        }
-    }
-    Ok(())
-}
-
-#[test]
-fn find_a_match() {
-    let mut result_line = Vec::new();
-    let mut lines = Vec::new();
-
-    lines.push(String::from("hogefuga"));
-    lines.push(String::from("foobar"));
-
-    let result = find_matches(&mut result_line, lines, "fuga");
-    assert!(result.is_ok());
-    assert_eq!(result_line, b"hogefuga\n");
 }
 
 fn main() -> Result<()> {
@@ -48,6 +26,6 @@ fn main() -> Result<()> {
     let lines = BufReader::new(file).lines();
     let lines = lines.map(|line| line.unwrap()).collect::<Vec<String>>();
     let writer = BufWriter::new(stdout());
-    find_matches(writer, lines, &args.pattern)?;
+    grrs::find_matches(writer, lines, &args.pattern)?;
     Ok(())
 }
